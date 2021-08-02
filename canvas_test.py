354dffb7615881e2c8bc73a1c173411d55dbbc8a
Colby England <englandcolby@gmail.com>
2021-08-02 08:49:28 -0500

    Prototype is functioning. TO-DO: add help window

--- canvas_test.py
+++ canvas_test.py
@@ -4,16 +4,62 @@ from tkinter.colorchooser import askcolor
 from tkinter.filedialog import asksaveasfilename
 import tkinter.tix as tix
 from PIL import ImageTk, Image, ImageGrab
+import requests
+from urllib.request import urlopen
+import uuid
+from io import BytesIO
 import ctypes
 ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
 
+# Define Contants for use on the microservice call
+SERVICE_URL = "https://www.weshavens.info:443/uploadV2"
+SERVICE_PATH = "/CanvasApp/"
+
+
+# Define tooltip constants
 URL_HELP = "Enter the URL\nfor the image\nyou wish to\nedit."
+
 BEGIN_HELP = "Start your project\nwith the image and size\nspecified above."
+
 IMAGE_SIZE_HELP = """Select the size you would like
                      your image to be. The first number is
                      the width and the the second number is
                      the height."""
+
+BRUSH_HELP = """This button toggles the
+brush tool on and off.
+When toggled on it will
+allow you to click and
+hold to draw on the picture."""
+
+LINE_HELP = """This button toggles the
+line tool on and off.
+When toggled on it will
+allow you to draw a line
+between two points by clicking
+at the first point, dragging the 
+mouse to the second point and
+releasing the mouse button."""
+
+COLOR_HELP = """This button opens a color
+select window. The color will then
+be used for the drawing tools.
+"""
+
+BRUSH_SIZE_HELP = """This slide changes the width
+of the brush tool. The units
+are in pixels and range from
+1 to 100.
+"""
+
+UNDO_HELP = """This button will remove
+edits from the canvas
+in reverse order of when
+they were made."""
+
+SAVE_HELP = """This button will open
+a dialog to save your image."""
 
 class CanvasPage():
 
@@ -24,6 +70,9 @@ class CanvasPage():
         self.x_pos, self.y_pos = None, None
         self.x1, self.y1, self.x2, self.y2 = None, None, None, None
         self.selected_color = [None, "Black"]
+
+        self.edit_num = 0
+        
 
         self.undo_stack = []
 
@@ -37,40 +86,85 @@ class CanvasPage():
 
         # create a frame to hold the drawing tool buttons
         self.button_frame = Frame(master)
-        self.button_frame.grid(row=1, column=0)
+        self.button_frame.grid(row=1, column=0, sticky='nsew')
+
+        self.button_frame.columnconfigure(0, weight=1)
+        self.button_frame.columnconfigure(3, weight=1)
+        self.button_frame.columnconfigure(6, weight=1)
+        self.button_frame.columnconfigure(9, weight=1)
 
         # intialize list of tool types
-        self.tools = ["brush", "line", "text"]
+        self.tools = ["brush", "line"]
 
         # create radio buttons of tool types
         self.selected_tool = StringVar(self.button_frame)
         self.selected_tool.set("brush")
 
+        row_count = 0
         for tool in self.tools:
             Radiobutton(self.button_frame,
                         text=tool,
                         variable=self.selected_tool,
                         value=tool,
                         command=self.show_choice
-            ).pack()
+            ).grid(row=row_count, column=1)
+            row_count += 1
         
+        # create tooltip for brush toggle
+        self.brush_help_icon = Label(self.button_frame, text="?")
+        self.brush_help = tix.Balloon(self.button_frame)
+        self.brush_help.bind_widget(self.brush_help_icon, balloonmsg=BRUSH_HELP)
+        self.brush_help_icon.grid(row=0, column=2)
+
+        # line tooltip
+        self.line_help_icon = Label(self.button_frame, text="?")
+        self.line_help = tix.Balloon(self.button_frame)
+        self.line_help.bind_widget(self.line_help_icon, balloonmsg=LINE_HELP)
+        self.line_help_icon.grid(row=1, column=2)
+        
+        # slider tooltip
+        self.brush_size_icon = Label(self.button_frame, text="?")
+        self.brush_size_help = tix.Balloon(self.button_frame)
+        self.brush_size_help.bind_widget(self.brush_size_icon, balloonmsg=BRUSH_SIZE_HELP)
+        self.brush_size_icon.grid(row=0, column=5)
+
+        # color chooser tooltip
+        self.color_icon = Label(self.button_frame, text="?")
+        self.color_help = tix.Balloon(self.button_frame)
+        self.color_help.bind_widget(self.color_icon, balloonmsg=COLOR_HELP)
+        self.color_icon.grid(row=1, column=5)
+
+        # undo tooltip
+        self.undo_help_icon = Label(self.button_frame, text="?")
+        self.undo_help = tix.Balloon(self.button_frame)
+        self.undo_help.bind_widget(self.undo_help_icon, balloonmsg=UNDO_HELP)
+        self.undo_help_icon.grid(row=0, column=8)
+
+        # save image tooltip
+        self.save_help_icon = Label(self.button_frame, text="?")
+        self.save_help = tix.Balloon(self.button_frame)
+        self.save_help.bind_widget(self.save_help_icon, balloonmsg=SAVE_HELP)
+        self.save_help_icon.grid(row=1, column=8)
+
         # create slider for line width
         self.line_scale = Scale(self.button_frame, from_=1, to=100, orient=HORIZONTAL)
-        self.line_scale.pack()
+        self.line_scale.grid(row=0, column=4)
 
         self.color_btn = Button(self.button_frame, text='Select a Color', command=self.choose_color, fg=self.selected_color[1])
-        self.color_btn.pack()
+        self.color_btn.grid(row=1, column=4)
 
-        self.copy_image = Button(self.button_frame, text='Copy Image', command= lambda: self.save_image(master, self.drawing_area))
-        self.copy_image.pack()
+        self.copy_image = Button(self.button_frame, text='Save Image', command= lambda: self.save_image(master, self.drawing_area))
+        self.copy_image.grid(row=1, column=7)
 
+        self.undo_btn = Button(self.button_frame, text='Undo', command=self.undo)
+        self.undo_btn.grid(row=0, column=7)
         # open the start menu ontop of the root menu
         self.open_start_menu()
 
         # bind events to the canvas
         self.drawing_area.bind("<Motion>", self.motion)
         self.drawing_area.bind("<ButtonPress-1>", self.left_mouse_down)
-        master.bind("<ButtonPress-1>", self.left_mouse_down)
+        # master.bind("<ButtonPress-1>", self.left_mouse_down)
         self.drawing_area.bind("<ButtonRelease-1>", self.left_mouse_up)
 
     def open_start_menu(self):
@@ -158,11 +252,27 @@ class CanvasPage():
         self.dimensions = self.image_size_string.get().split(sep='x')
         self.drawing_area.config(width=int(self.dimensions[0]), height=int(self.dimensions[1]))
         self.canvas_frame.config(width=int(self.dimensions[0]), height=int(self.dimensions[1]))
-        self.open_image(self.url.get())
+        # self.remote_image_url = self.request_image(self.url.get())['url']
+        self.remote_image_url = 'https://pernetyp.sirv.com/CanvasApp/3fcd2d4c-7fb4-4919-851b-2426605178b3'
+        self.open_image(self.remote_image_url, self.dimensions[0], self.dimensions[1])
 
-    def open_image(self, img_url):
+    def request_image(self, image_url):
+        self.request_params = {"url": image_url,
+                  "path": SERVICE_PATH,
+                  "file": str(uuid.uuid4())
+        }
+        
+        request = requests.post(SERVICE_URL, data=self.request_params)
 
-        self.img = ImageTk.PhotoImage(Image.open(img_url))  # PIL solution
+        return request.json()
+
+    def open_image(self, img_url, width, height):
+
+        self.modified_url = img_url + "?scale.width=" + width + "&scale.height=" + height + "&scale.option=ignore"
+
+        self.raw_image = urlopen(self.modified_url).read()
+
+        self.img = ImageTk.PhotoImage(Image.open(BytesIO(self.raw_image)))  # PIL solution
         self.drawing_area.create_image(0, 0, anchor=NW, image=self.img)
         self.splash_screen.destroy()
 
@@ -173,7 +283,7 @@ class CanvasPage():
         self.x1 = event.x
         self.y1 = event.y
 
-        print(f'x: {self.x1}, y: {self.y1}')
+        # print(f'x: {self.x1}, y: {self.y1}')
 
     def left_mouse_up(self, event=None):
 
@@ -189,6 +299,10 @@ class CanvasPage():
 
         if current_tool == "line":
             self.line_draw(event)
+        
+        self.undo_stack.append("edit#" + str(self.edit_num))
+        print(self.undo_stack)
+        self.edit_num += 1
 
     def motion(self, event=None):
         # print("motion detected")
@@ -198,7 +312,7 @@ class CanvasPage():
     def line_draw(self, event=None):
 
         if None not in (self.x1, self.y1, self.x2, self.y2):
-            event.widget.create_line(self.x1, self.y1, self.x2, self.y2, smooth=True, fill=self.selected_color[1])
+            event.widget.create_line(self.x1, self.y1, self.x2, self.y2, smooth=True, fill=self.selected_color[1], tags=("edit#" + str(self.edit_num)))
 
     def brush_draw(self, event=None):
 
@@ -214,7 +328,8 @@ class CanvasPage():
                                         width=self.line_scale.get(), 
                                         capstyle=ROUND, 
                                         joinstyle=ROUND,
-                                        fill=self.selected_color[1])
+                                        fill=self.selected_color[1],
+                                        tags=("edit#" + str(self.edit_num)))
                 
             self.x_pos = event.x
             self.y_pos = event.y
@@ -259,6 +374,10 @@ class CanvasPage():
         ImageGrab.grab().crop((self.x, self.y, self.x + self.widget_x2, self.y + self.widget_y2)).save(file_name)
         # .crop((self.x, self.y, self.x + self.widget_x2, self.y + self.widget_y2))
 
+    def undo(self):
+
+        if len(self.undo_stack) > 0:
+            self.drawing_area.delete(self.undo_stack.pop())
 
 # Create main window and set to full-screen
 root = tix.Tk()
